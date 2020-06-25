04af5c1ab9fab1334fbbf710e46d5df0701ffb30
Crazy <744890753@qq.com>
2020-06-25 13:28:42 +0800

    创建项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <calendar/>
   </div>
 </template>
+<script>
+    import calendar from './components/calendar/calendar'
+  export default {
+        components:{
+            calendar
+        }
+  }
+</script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app{
+    width:500px;
   }
-}
 </style>
